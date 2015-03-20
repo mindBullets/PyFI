a900c22b8c96f7b5e998124084a596ecc8aca030
MindBullets <jbalacy@gmail.com>
2015-03-20 11:17:37 -0700

    cleaned up comments

--- tuples.py
+++ tuples.py
@@ -1,18 +1,16 @@
 __author__ = 'MindBullets'
 txt = 'but soft what light in yonder window breaks'
-# build a list of tuples
-
 txt = txt.split()
+# build a list of tuples
 t = list()
-
+# add word length, word for each tuple then reverse it
 for word in txt:
     t.append((len(word), word))
-
 t.sort(reverse=True)
 print(t)
 
+# extract only the word then print
 lst = list()
 for wordLen, word in t:
     lst.append(word)
-
 print(lst)
