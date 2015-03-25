e9dae8b2cee9b335171c8f6cc43c3938f7f0a0a1
MindBullets <jbalacy@gmail.com>
2015-03-25 07:39:30 -0700

    Open a socket to download a jpg
    The key in Python 3 is to store the data in a bytes object start to finish. When searching for the delimiter CRLF, pass a bytes object into the bytes.find() function.

--- networkedPrograms.py
+++ networkedPrograms.py
@@ -1,6 +1,8 @@
 __author__ = 'MindBullets'
 import socket
+import time
 
+"""Super basic web browser
 mySock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 mySock.connect(('www.py4inf.com', 80))
 mySock.send(bytes('GET http://www.py4inf.com/code/romeo.txt HTTP/1.0\n\n', 'UTF-8'))
@@ -11,3 +13,37 @@ while True:
         break
     print(data)
 mySock.close()
+End super basic web browser"""
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(('www.py4inf.com', 80))
+s.send(bytes('GET http://www.py4inf.com/cover.jpg HTTP/1.0\n\n', 'UTF-8'))
+
+count = 0
+picture = bytes()
+
+while True:
+    data = s.recv(5120)
+    if len(data) < 1:
+        break
+    time.sleep(0.25)
+    picture += data
+    count += len(data)
+    print('LENGTH: {0} COUNT: {1}'.format(len(data), count))
+s.close()
+
+pos = picture.find(bytes('\r\n\r\n', 'utf8')) + 4 #find two carriage return new lines ie., CRLF 4 characters
+
+print("Header length: {0} characters".format(pos))
+
+picture = picture[pos:]
+#print("Remaining data")
+#print(picture)
+
+try:
+    fhand = open("myPicture.jpg", "wb") # wb is write binary
+except:
+    print("Failed to open file: {0}".format(fhand))
+
+fhand.write(picture)
+fhand.close()
